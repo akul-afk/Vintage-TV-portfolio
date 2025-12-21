db89aafec8c93d2d954cf1a74302e9879cf5afaf
akul-afk <akulchutia@gmail.com>
2025-12-21 20:00:52 +0530

    w

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+
+const EMAILJS_CONFIG = {
+    PUBLIC_KEY: "AVyB_eDwsaTLnryjR",     // From Account tab
+    SERVICE_ID: "service_akul_afk",     // From Email Services tab
+    TEMPLATE_ID: "template_akul_afk"    // From Email Templates tab
+};
